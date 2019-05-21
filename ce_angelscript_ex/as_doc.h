a33d99bd54b4d4dce487b2737b118e060966dad6
Carsten Arnholm <arnholm@arnholm.org>
2019-05-21 19:42:43 +0200

    Changed license for ce_angelscript_ex

--- ce_angelscript_ex/as_doc.h
+++ ce_angelscript_ex/as_doc.h
@@ -6,6 +6,31 @@
 #include <memory>
 #include "cf_syslib/xml_node.h"
 #include "as_typeid.h"
+// BeginLicense:
+// Part of: cpde_syslibs - Cross Platform Development Environment, system libraries
+// Copyright (C) 2017 Carsten Arnholm
+// All rights reserved
+//
+// Permission is granted to anyone to use this software for any
+// purpose, including commercial applications, and to alter it and
+// redistribute it freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you
+//    must not claim that you wrote the original software. If you use
+//    this software in a product, an acknowledgment in the product
+//    documentation would be appreciated.
+//
+// 2. Altered source versions must be plainly marked as such, and
+//    must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
+//
+// This file is provided "AS IS" with NO WARRANTY OF ANY KIND,
+// INCLUDING THE WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE.
+// EndLicense:
+
 #include "as_description.h"
 class as_xml;
 
--- ce_angelscript_ex/as_include_callback.cpp
+++ ce_angelscript_ex/as_include_callback.cpp
@@ -3,16 +3,25 @@
 // Copyright (C) 2017 Carsten Arnholm
 // All rights reserved
 //
-// This file may be used under the terms of either the GNU General
-// Public License version 2 or 3 (at your option) as published by the
-// Free Software Foundation and appearing in the files LICENSE.GPL2
-// and LICENSE.GPL3 included in the packaging of this file.
+// Permission is granted to anyone to use this software for any
+// purpose, including commercial applications, and to alter it and
+// redistribute it freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you
+//    must not claim that you wrote the original software. If you use
+//    this software in a product, an acknowledgment in the product
+//    documentation would be appreciated.
+//
+// 2. Altered source versions must be plainly marked as such, and
+//    must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
 //
 // This file is provided "AS IS" with NO WARRANTY OF ANY KIND,
 // INCLUDING THE WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR
 // A PARTICULAR PURPOSE.
 // EndLicense:
-
 #include "as_include_callback.h"
 #include "as_file.h"
 #include <string>
