b3c3d07a947f87de65a15fa57513ffaaa7450584
Carsten Arnholm <arnholm@arnholm.org>
2019-05-28 21:18:05 +0200

    introduced replace_substring_copy and replace_substring_in_file

--- cf_syslib/replace_substring.h
+++ cf_syslib/replace_substring.h
@@ -8,10 +8,14 @@
 
 // most efficient version with in-place replacement
 CF_SYSLIB_PUBLIC
-void replace_substring(std::string& line, std::string& oldString, std::string& newString);
+void replace_substring(std::string& line, const std::string& oldString, const std::string& newString);
 
 // convenience variant retuns the modified string. Involves an extra copy of the input string
 CF_SYSLIB_PUBLIC
-std::string replace_substring(const std::string& line, std::string& oldString, std::string& newString);
+std::string replace_substring_copy(const std::string& line, const std::string& oldString, const std::string& newString);
+
+// replace string in given file, throw exception if file can not be opened for both read and write
+CF_SYSLIB_PUBLIC
+void replace_substring_in_file(const std::string& file_path, const std::string& oldString, const std::string& newString);
 
 #endif // REPLACE_SUBSTRING_H
