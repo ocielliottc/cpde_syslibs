64b31da595a8dc97aa61590044b92ee557cc90a1
Carsten Arnholm <arnholm@arnholm.org>
2019-07-31 08:18:43 +0200

    Cleanup using replace_substring_inplace && replace_substring

--- cf_syslib/replace_substring.cpp
+++ cf_syslib/replace_substring.cpp
@@ -1,8 +1,6 @@
 #include "replace_substring.h"
-#include <list>
-#include <fstream>
 
-void replace_substring(std::string& line, const std::string& oldString, const std::string& newString)
+void replace_substring_inplace(std::string& line, const std::string& oldString, const std::string& newString)
 {
    const size_t oldSize = oldString.length();
 
@@ -28,43 +26,9 @@ void replace_substring(std::string& line, const std::string& oldString, const st
    }
 }
 
-std::string replace_substring_copy(const std::string& line, const std::string& oldString, const std::string& newString)
+std::string replace_substring(const std::string& line, const std::string& oldString, const std::string& newString)
 {
    std::string line_copy(line);
-   replace_substring(line_copy,oldString,newString);
+   replace_substring_inplace(line_copy,oldString,newString);
    return std::move(line_copy);
-}
-
-
-void replace_substring_in_file(const std::string& file_path, const std::string& oldString, const std::string& newString)
-{
-   // perform in-place replacement
-
-   // first read the file and store in list
-   std::list<std::string> lines;
-   {
-      std::ifstream in(file_path);
-      if(in.is_open()) {
-         std::string line;
-         while(std::getline(in,line)) {
-            lines.push_back(replace_substring_copy(line,oldString,newString));
-         }
-      }
-      else {
-         throw std::runtime_error("File could not be opened for read: " + file_path);
-      }
-   }
-
-   // write to same file
-   {
-      std::ofstream out(file_path);
-      if(out.is_open()) {
-         for(auto& line : lines) {
-            out << line << std::endl;
-         }
-      }
-      else {
-         throw std::runtime_error("File could not be opened for write: " + file_path);
-      }
-   }
 }
